87b3b0c3292396872951be99823f27578a59f64d
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2014-11-04 22:24:43 +0000

    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/googlecustomsearch/trunk@5873 7b40dde9-fc51-43d4-b609-63a66f0a0cee

---
+++ javascript/GoogleCustomSearchPage.js
@@ -0,0 +1,22 @@
+
+
+var GoogleCustomSearchPage = {
+
+	/**
+	 * cx "key"... set by PHP
+	 * @var String
+	 */
+	cxKey: "",
+
+	init: function(){
+		var gcse = document.createElement('script');
+		gcse.type = 'text/javascript';
+		gcse.async = true;
+		gcse.src = (document.location.protocol == 'https' ? 'https:' : 'http:') +
+				'//www.google.com/cse/cse.js?cx=' + this.cxKey;
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
+	}
+
+}
+
+GoogleCustomSearchPage.init();
